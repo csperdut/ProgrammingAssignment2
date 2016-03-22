6e7f0c609d2d66bd9b1b391aeb89bde984be3cd9
Amber Carver <amber.carver@ucdenver.edu>
2016-03-21 18:59:24 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this exercise was to write two functions.
+## One function needed to create and temporarily store a square matrix,
+## and the other function needed to calculate the inverse of that matrix. 
 
-## Write a short comment describing this function
+## FUNCTION 1: This function creates a square matrix (a matrix with an equal
+## number of columns and rows and--for the purposes of testing this function--
+## a unique value in each cell.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  matrix(data=1:4,ncol=x,nrow=x)
+  
 }
 
 
-## Write a short comment describing this function
+## FUNCTION 2: This function uses function 1 internally, and it
+## returns a matrix that is the inverse of the first matrix, using the 
+## solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  solve(makeCacheMatrix(x))
 }
